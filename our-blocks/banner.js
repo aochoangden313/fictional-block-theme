9b22299dfdcb933b3042786ccfcc84816d369e88
Le Van Quan <levanquan@192.168.1.104>
2025-01-22 10:23:17 +0700

    182. Making Our Block Editable

--- our-blocks/banner.js
+++ our-blocks/banner.js
@@ -1,3 +1,5 @@
+import { InnerBlocks } from "@wordpress/block-editor"
+
 wp.blocks.registerBlockType("ourblocktheme/banner", {
     title: "Banner",
     edit: Editcomponent,
@@ -5,19 +7,32 @@ wp.blocks.registerBlockType("ourblocktheme/banner", {
 })
 
 function Editcomponent() {
+    const useMeLater = (
+        <>
+            <h1 className="headline headline--large">Welcome! 2222222</h1>
+            <h2 className="headline headline--medium">We think you&rsquo;ll like it here.</h2>
+            <h3 className="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
+            <a href="#" className="btn btn--large btn--blue">Find Your Major</a>
+        </>
+    );
+
     return (
         <div className="page-banner">
             <div className="page-banner__bg-image" style={{ backgroundImage: "url('/wp-content/themes/fictional-block-theme/images/library-hero.jpg')" }}></div>
             <div className="page-banner__content container t-center c-white">
-                <h1 className="headline headline--large">Welcome! 2222222</h1>
-                <h2 className="headline headline--medium">We think you&rsquo;ll like it here.</h2>
-                <h3 className="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
-                <a href="#" className="btn btn--large btn--blue">Find Your Major</a>
+                <InnerBlocks allowedBlocks={["coe/paragraph", "core/heading", "core/list"]}></InnerBlocks>
             </div>
         </div>
     )
 }
 
 function SaveCombonent() {
-    return <p>This is our block component</p>
+    return (
+        <div className="page-banner">
+            <div className="page-banner__bg-image" style={{ backgroundImage: "url('/wp-content/themes/fictional-block-theme/images/library-hero.jpg')" }}></div>
+            <div className="page-banner__content container t-center c-white">
+                <InnerBlocks.Content/>
+            </div>
+        </div>
+    )
 }
